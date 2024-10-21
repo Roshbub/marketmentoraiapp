47253c16fc8db819bc342bd7f3c1586746c9b560
Roshbub <129534256+Roshbub@users.noreply.github.com>
2024-10-21 10:37:45 -0400

    Update mainprogram.py(1)

--- mainprogram.py
+++ mainprogram.py
@@ -1,23 +1,14 @@
-import os
-import subprocess
-import sys
-
-# Function to install missing packages
-def install_package(package):
-    try:
-        __import__(package)
-    except ImportError:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-# List of required packages
-required_packages = [
-    'yfinance', 'yahoo_fin', 'sklearn', 'textblob', 'matplotlib', 'seaborn',
-    'pypfopt', 'tqdm', 'statsmodels'
-]
-
-# Install each package if missing
-for package in required_packages:
-    install_package(package)
+import install
+pip install yfinance
+pip install yahoo_fin
+pip install sklearn
+pip install textblob
+pip install matplotlib
+pip install seaborn
+pip install pypfopt
+pip install tqdm
+pip install statsmodels
+pip install streamlit
 
 
 # Import packages
